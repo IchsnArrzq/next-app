cad23263ec84a754a01c508c921f1b32f9f5ee2f
IchsnArrzq <op.matra.hillindo@gmail.com>
2022-07-05 17:08:00 +0700

    machine

--- src/pages/activity/machine/index.jsx
+++ src/pages/activity/machine/index.jsx
@@ -1,20 +1,15 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import axios from '@/lib/axios'
-import { Badge, Button, Card, Checkbox, Collapse, Grid, Group, Stack, Table, Title } from '@mantine/core'
+import { Badge, Button, Card, Checkbox, Collapse, Grid, Group, Paper, Stack, Table, Title } from '@mantine/core'
 import React, { useEffect, useState } from 'react'
 
 export default function MachineIndex({ data }) {
     const [opened, setOpen] = useState(false);
     const [machines, setMachines] = useState(data);
-    const ShowDevices = async id => {
-        // const { data } = await axios.get(`/machine/${id}`)
-        console.log(machines, id)
+    const HandleCheck = index => {
+        machines[index].status = machines[index].status ? false : true
+        setMachines([...machines])
     }
-    const HideDevices = id => {
-    }
-    useEffect(() => {
-        console.log(machines)
-    }, [])
     return (
         <Grid columns={12}>
             {/* content... */}
@@ -43,10 +38,10 @@ export default function MachineIndex({ data }) {
                                         {
                                             machines.map((machine, index) => {
                                                 return (
-                                                    <tr key={index}>
-                                                        <td><Checkbox checked={machines[index].status} onChange={() => ShowDevices(machine.id)} /></td>
+                                                    <tr key={index} htmlFor={index}>
+                                                        <td><Checkbox id={index} checked={machines[index].status} onChange={() => HandleCheck(index)} /></td>
                                                         <td>{machine.name}</td>
-                                                        <td>{true ? <Badge color={'teal'} children='run' /> : <Badge color={'red'} children='stop' />}</td>
+                                                        <td>{machine.status ? <Badge color={'teal'} children='run' /> : <Badge color={'red'} children='stop' />}</td>
                                                     </tr>
                                                 )
                                             })
@@ -69,7 +64,50 @@ export default function MachineIndex({ data }) {
                         </Group>
                     </Card>
                     <Card p={'xs'}>
-
+                        <Stack>
+                            {
+                                machines.filter(machine => {
+                                    return machine.status == true
+                                }).map((item, key) => {
+                                    return (
+                                        <div key={key}>
+                                            <Stack>
+                                                <Title order={5}>{item.name}</Title>
+                                                {
+                                                    item.planning_machines.map((planning_machine, id) => {
+                                                        return (
+                                                            <Grid columns={12}>
+                                                                <Grid.Col span={8}>
+                                                                    <Paper shadow={'sm'} key={id} p="md">
+                                                                        <Group position='apart'>
+                                                                            <Title order={5} >
+                                                                                {planning_machine.product.part_name}
+                                                                            </Title>
+                                                                            <Title order={5}>
+                                                                                OOE: xx%
+                                                                            </Title>
+                                                                        </Group>
+                                                                    </Paper>
+                                                                </Grid.Col>
+                                                                <Grid.Col span={4}>
+                                                                    <Paper shadow={'sm'} key={id} p="md">
+                                                                        <Group position='center'>
+                                                                            <Title order={5} >
+                                                                                line stop {item.name}
+                                                                            </Title>
+                                                                        </Group>
+                                                                    </Paper>
+                                                                </Grid.Col>
+                                                            </Grid>
+                                                        )
+                                                    })
+                                                }
+                                            </Stack>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </Stack>
                     </Card>
                 </Stack>
             </Grid.Col>
@@ -79,7 +117,10 @@ export default function MachineIndex({ data }) {
 
 MachineIndex.getLayout = page => <AppLayout children={page} />
 MachineIndex.getInitialProps = async () => {
-    const { data } = await axios.get('machine')
+    let { data } = await axios.get('machine')
+    for (let index = 0; index < data.length; index++) {
+        data[index].status = false;
+    }
     return {
         data
     }
